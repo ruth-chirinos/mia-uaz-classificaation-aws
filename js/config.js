c543b9b587157affad70cba3fdb33205d25026a9
Ruth Chirinos <ruth.chirinos@mojix.com>
2021-10-02 20:32:47 -0400

    Added cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ApplicationClientUNIR-UAZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '39k8t7fafbq8vhq31k3enegi3l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
