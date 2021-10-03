414ff273d3d27dc94b9a339df94cd95b93ddf517
Ruth Chirinos <ruth.chirinos@mojix.com>
2021-10-02 22:35:35 -0400

    User PoolId

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'ApplicationClientUNIR-UAZ', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-2_bLyy4ccfQ', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '39k8t7fafbq8vhq31k3enegi3l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
