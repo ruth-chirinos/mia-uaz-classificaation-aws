758ddf84034569a1c910f07919cf81b86e078db9
Ruth Chirinos <ruth.chirinos@mojix.com>
2021-10-04 22:45:40 -0400

    upload file

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const imageForm = document.querySelector("#imageForm")
 const imageInput = document.querySelector("#imageInput")
 
-imageForm.addEventListener("submit", async event => {
+imageForm.addEventListener("submitId", async event => {
   event.preventDefault()
   const file = imageInput.files[0]
 
